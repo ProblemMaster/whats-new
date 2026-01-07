3afdbf44a46a021d83234d483cc748d80fd99fdd
ProblemMaster <63835@gymnasium.ax>
2026-01-07 11:35:22 +0200

    lagt till bilder i public och ändrat i vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,45 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      manifest: {
+        "theme_color": "#0F172A",
+        "background_color": "#f5f8fa",
+        "display": "standalone",
+        "scope": "/",
+        "start_url": "/",
+        "name": "What's New - Vue News Aggregator Site",
+        "short_name": "What's New",
+        "description": "A news aggregator pulling news items from News API.",
+        "icons": [
+          {
+            "src": "/icon-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "/icon-256x256.png",
+            "sizes": "256x256",
+            "type": "image/png"
+          },
+          {
+            "src": "/icon-384x384.png",
+            "sizes": "384x384",
+            "type": "image/png"
+          },
+          {
+            "src": "/icon-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+          }
+        ]
+      }
+    })
+  ],
 })
+
